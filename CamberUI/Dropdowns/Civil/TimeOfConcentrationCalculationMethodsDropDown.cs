d51de7597ebbd7fabbe0736f45ae1e1380df1f7c
Zachri Jensen <zjensen@kittelson.com>
2021-10-25 21:32:19 -0700

    Added ability to get/set reference surface, pipe network, and structure. Updated documentation to refer to "a" catchment instead of "the" catchment. Added dropdown node for time of concentration calculation methods.

---
+++ CamberUI/Dropdowns/Civil/TimeOfConcentrationCalculationMethodsDropDown.cs
@@ -0,0 +1,54 @@
+﻿#region references
+using System.Collections.Generic;
+using CoreNodeModels;
+using Dynamo.Graph.Nodes;
+using Dynamo.Utilities;
+using ProtoCore.AST.AssociativeAST;
+using Newtonsoft.Json;
+using System.Linq;
+#endregion
+
+namespace Camber.UI
+{
+    [NodeName("Time of Concentration Calculation Methods")]
+    [NodeCategory("Camber.Civil 3D.Catchment")]
+    [NodeDescription("Select time of concentration calculation method.")]
+    [IsDesignScriptCompatible]
+    public class TimeOfConcentrationCalculationMethodsDropDown : DSDropDownBase
+    {
+        public TimeOfConcentrationCalculationMethodsDropDown() : base("calculationMethod") { }
+
+        [JsonConstructor]
+        public TimeOfConcentrationCalculationMethodsDropDown(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base("calculationMethod", inPorts, outPorts) { }
+
+        protected override SelectionState PopulateItemsCore(string currentSelection)
+        {
+            Items.Clear();
+
+            var newItems = new List<DynamoDropDownItem>()
+            {
+                new DynamoDropDownItem("User-defined", "CalculationMethodUserDefined"),
+                new DynamoDropDownItem("TR-55", "CalculationMethodTR55")
+            };
+
+            Items.AddRange(newItems);
+            Items = Items.OrderBy(x => x.Name).ToObservableCollection();
+
+            SelectedIndex = -1;
+            return SelectionState.Restore;
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            if (Items.Count == 0 || SelectedIndex == -1)
+            {
+                return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
+            }
+
+            var stringNode = AstFactory.BuildStringNode((string)Items[SelectedIndex].Item);
+            var assign = AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), stringNode);
+
+            return new List<AssociativeNode> { assign };
+        }
+    }
+}
