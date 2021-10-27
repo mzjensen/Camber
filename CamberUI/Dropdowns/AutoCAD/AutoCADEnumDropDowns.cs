4ba16f636e3ac17985424bb5b9b1d99d7799819b
Zachri Jensen <zjensen@kittelson.com>
2021-10-26 22:48:19 -0700

    Added dropdown for block units.

---
+++ CamberUI/Dropdowns/AutoCAD/AutoCADEnumDropDowns.cs
@@ -0,0 +1,24 @@
+﻿#region references
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using acDb = Autodesk.AutoCAD.DatabaseServices;
+using Dynamo.Graph.Nodes;
+#endregion
+
+namespace Camber.UI
+{
+    [NodeName("Block Units")]
+    [NodeCategory("Camber.AutoCAD.BlockExtensions")]
+    [NodeDescription("Select Block units.")]
+    [IsDesignScriptCompatible]
+    public class BlockUnitsDropDown : EnumDropDownBase
+    {
+        private const string OutputName = "units";
+
+        public BlockUnitsDropDown() : base(OutputName, typeof(acDb.UnitsValue)) { }
+
+        [JsonConstructor]
+        public BlockUnitsDropDown(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts)
+            : base(OutputName, typeof(acDb.UnitsValue), inPorts, outPorts) { }
+    }
+}
