4147642016ca4126955652bf7ac0456f0daac421
Zachri Jensen <zjensen@kittelson.com>
2022-02-19 22:19:33 -0800

    Started playing around with assembly late binding

---
+++ Camber/Civil/Data Shortcuts/DSAssemblyLoader.cs
@@ -0,0 +1,29 @@
+﻿#region references
+using System;
+using System.Reflection;
+using Camber.Utils;
+#endregion
+
+namespace Camber.Civil.DataShortcuts
+{
+    internal static class DSAssemblyLoader
+    {
+        /// <summary>
+        /// Attempts to load the AeccDataShortcutMgd assembly.
+        /// </summary>
+        /// <returns></returns>
+        internal static Assembly LoadDSAssembly()
+        {
+            string appVersion = Globals.GetCurrentVersion().ToString();
+            string path = "C:\\Program Files\\Autodesk\\AutoCAD " + appVersion + "\\C3D\\AeccDataShortcutMgd.dll";
+            try
+            {
+                return Assembly.LoadFrom(path);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
