2f6bc2ff9fc0da9a3c8bcc49c91693a329fa989d
Zachri Jensen <zjensen@kittelson.com>
2022-02-21 21:52:15 -0800

    Added dropdowns for pressure part domains and types

--- CamberUI/Civil/CivilEnumDropDowns.cs
+++ CamberUI/Civil/CivilEnumDropDowns.cs
@@ -1,6 +1,7 @@
 ﻿#region references
 using System.Collections.Generic;
 using civDs = Autodesk.Civil.DataShortcuts;
+using civDb = Autodesk.Civil.DatabaseServices;
 using Newtonsoft.Json;
 using Dynamo.Graph.Nodes;
 #endregion
@@ -20,5 +21,35 @@ namespace Camber.UI
         [JsonConstructor]
         public DataShortcutEntityTypesDropDown(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts)
             : base(OutputName, typeof(civDs.DataShortcutEntityType), inPorts, outPorts) { }
+    }
+
+    [NodeName("Pressure Part Domains")]
+    [NodeCategory("Camber.Civil 3D.CivilObjects.Pressure Networks.PressurePartsList")]
+    [NodeDescription("Select Pressure Part domain.")]
+    [IsDesignScriptCompatible]
+    public class PressurePartDomainDropDown : EnumDropDownBase
+    {
+        private const string OutputName = "pressurePartDomain";
+
+        public PressurePartDomainDropDown() : base(OutputName, typeof(civDb.PressurePartDomainType)) { }
+
+        [JsonConstructor]
+        public PressurePartDomainDropDown(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts)
+            : base(OutputName, typeof(civDb.PressurePartDomainType), inPorts, outPorts) { }
+    }
+
+    [NodeName("Pressure Part Types")]
+    [NodeCategory("Camber.Civil 3D.CivilObjects.Pressure Networks.PressurePartsList")]
+    [NodeDescription("Select Pressure Part type.")]
+    [IsDesignScriptCompatible]
+    public class PressurePartTypeDropDown : EnumDropDownBase
+    {
+        private const string OutputName = "pressurePartType";
+
+        public PressurePartTypeDropDown() : base(OutputName, typeof(civDb.PressurePartType), true) { }
+
+        [JsonConstructor]
+        public PressurePartTypeDropDown(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts)
+            : base(OutputName, typeof(civDb.PressurePartType), inPorts, outPorts) { }
     }
 }
