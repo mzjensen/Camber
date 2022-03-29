9a94497848f9da19ed76e0ea6e8423a6669fb020
Zachri Jensen <mzjensen@users.noreply.github.com>
2022-03-29 16:43:53 -0700

    Fixes #88

--- Camber/AutoCAD/Document.cs
+++ Camber/AutoCAD/Document.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using acDb = Autodesk.AutoCAD.DatabaseServices;
 using acDynNodes = Autodesk.AutoCAD.DynamoNodes;
 using acApp = Autodesk.AutoCAD.ApplicationServices;
 using Dynamo.Graph.Nodes;
@@ -13,10 +14,52 @@ namespace Camber.AutoCAD
 {
     public static class Document
     {
-        #region properties
+        #region query methods
+        /// <summary>
+        /// Gets if a Document is a named file on the disk (as opposed to a new drawing that has not yet been saved).
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static bool IsNamedDrawing(acDynNodes.Document document)
+        {
+            return document.AcDocument.IsNamedDrawing;
+        }
+
+        /// <summary>
+        /// Gets if a Document is read-only.
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static bool IsReadOnly(acDynNodes.Document document)
+        {
+            return document.AcDocument.IsReadOnly;
+        }
+
+        /// <summary>
+        /// Gets the version of a Document when it was last saved with the current session.
+        /// Returns null if the Document has not been saved with the current session.
+        /// Returns 0 for any version prior to 2004.
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static int? LastSavedVersion(acDynNodes.Document document)
+            => GetDatabaseVersionInfo(document.AcDocument.Database, true);
+
+        /// <summary>
+        /// Gets the version of a Document when it was first opened.
+        /// Returns 0 for any version prior to 2004.
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static int? OriginalVersion(acDynNodes.Document document)
+            => GetDatabaseVersionInfo(document.AcDocument.Database, false);
         #endregion
 
-        #region methods
+        #region action methods
         /// <summary>
         /// Executes a particular command string. Returns true if the command was executed successfully and false otherwise.
         /// Ensure that a space or \n is included at the end of any string that you wish to be executed.
@@ -73,28 +116,55 @@ namespace Camber.AutoCAD
                 throw new InvalidOperationException(e.Message);
             }
         }
-
-        /// <summary>
-        /// Gets if a Document is a named file on the disk (as opposed to a new drawing that has not yet been saved).
-        /// </summary>
-        /// <param name="document"></param>
-        /// <returns></returns>
-        [NodeCategory("Query")]
-        public static bool IsNamedDrawing(acDynNodes.Document document)
-        {
-            return document.AcDocument.IsNamedDrawing;
-        }
-
-        /// <summary>
-        /// Gets if a Document is read-only.
-        /// </summary>
-        /// <param name="document"></param>
-        /// <returns></returns>
-        [NodeCategory("Query")]
-        public static bool IsReadOnly(acDynNodes.Document document)
-        {
-            return document.AcDocument.IsReadOnly;
-        }
         #endregion
+
+        /// <summary>
+        /// Gets the DWG version of a database.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="getLastSaved">
+        /// True = get the version that was last saved by the current session.
+        /// False = get the original file version when the drawing was opened and prior to saving.
+        /// </param>
+        /// <returns>
+        /// Returns null if the database has not been saved in the current session.
+        /// Returns 0 if the DWG version is prior to 2004.
+        /// </returns>
+        internal static int? GetDatabaseVersionInfo(acDb.Database database, bool getLastSaved)
+        {
+            acDb.DwgVersion dwgVersion;
+
+            if (getLastSaved)
+            {
+                // The version that was last saved by the current session.
+                // This will return MC0To0 if the drawing has not been saved in the current session,
+                // even if the drawing has previously been saved to disk.
+                dwgVersion = database.LastSavedAsVersion;
+            }
+            else
+            {
+                // The version of the file when it was opened.
+                // This will return AC1032 if the database in the current session has never been saved.
+                dwgVersion = database.OriginalFileVersion;
+            }
+
+            switch (dwgVersion)
+            {
+                case acDb.DwgVersion.AC1032:
+                    return 2018;
+                case acDb.DwgVersion.AC1027:
+                    return 2013;
+                case acDb.DwgVersion.AC1024:
+                    return 2010;
+                case acDb.DwgVersion.AC1021:
+                    return 2007;
+                case acDb.DwgVersion.AC1800:
+                    return 2004;
+                case acDb.DwgVersion.MC0To0:
+                    return null;
+                default:
+                    return 0;
+            }
+        }
     }
 }
