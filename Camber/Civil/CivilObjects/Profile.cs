6e76d6f3703d26628926a1675d5121fc73a46e01
Zachri Jensen <mzjensen@users.noreply.github.com>
2022-03-30 14:40:51 -0700

    Changed Profile class to static

--- Camber/Civil/CivilObjects/Profile.cs
+++ Camber/Civil/CivilObjects/Profile.cs
@@ -4,35 +4,26 @@ using acDynNodes = Autodesk.AutoCAD.DynamoNodes;
 using acDynApp = Autodesk.AutoCAD.DynamoApp.Services;
 using civDynNodes = Autodesk.Civil.DynamoNodes;
 using AeccProfile = Autodesk.Civil.DatabaseServices.Profile;
-using DynamoServices;
 #endregion
 
 namespace Camber.Civil.CivilObjects
 {
-    [RegisterForTrace]
-    public sealed class Profile
+    public static class Profile
     {
-        #region properties
-        #endregion
-
-        #region constructors
-        private Profile() { }
-        #endregion
-
         #region methods
         /// <summary>
         /// Gets a Dynamo-wrapped Profile by Object ID.
         /// </summary>
-        /// <param name="oid"></param>
+        /// <param name="profId"></param>
         /// <returns></returns>
-        internal static civDynNodes.Profile GetFromObjectId(acDb.ObjectId oid)
+        internal static civDynNodes.Profile GetFromObjectId(acDb.ObjectId profId)
         {
             acDynNodes.Document document = acDynNodes.Document.Current;
             try
             {
                 using (acDynApp.DocumentContext ctx = new acDynApp.DocumentContext(document.AcDocument))
                 {
-                    var aeccProfile = ctx.Transaction.GetObject(oid, acDb.OpenMode.ForWrite) as AeccProfile;
+                    var aeccProfile = ctx.Transaction.GetObject(profId, acDb.OpenMode.ForWrite) as AeccProfile;
                     var parentAlign = Alignment.GetByObjectId(aeccProfile.AlignmentId);
                     return parentAlign.ProfileByName(aeccProfile.Name);
                 }
