289bfe68fc8f54bba1450b9fb567dc431efb77aa
Zachri Jensen <zjensen@woodrodgers.com>
2022-05-16 22:56:25 -0700

    Started work on surfaces

---
+++ Camber/Civil/CivilObjects/Surfaces/Surface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Civil.DatabaseServices;
+using acDynNodes = Autodesk.Civil.DynamoNodes;
+using AeccSurface = Autodesk.Civil.DatabaseServices.Surface;
+
+namespace Camber.Civil.CivilObjects.Surfaces
+{
+    public static class Surface
+    {
+        #region properties
+        #endregion
+
+        #region methods
+        #endregion
+    }
+}
