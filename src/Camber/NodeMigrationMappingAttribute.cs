df7a5d38d9c8090f7f389222b645103944104ecd
Zachri Jensen <zachrijensen@gmail.com>
2024-10-07 09:49:41 -0700

    Create NodeMigrationMappingAttribute.cs

---
+++ src/Camber/NodeMigrationMappingAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.DesignScript.Runtime;
+using System;
+
+namespace Camber
+{
+    [SupressImportIntoVM]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class NodeMigrationMappingAttribute : Attribute
+    {
+        public string OldName { get; }
+        public string NewName { get; }
+
+        public NodeMigrationMappingAttribute(string oldName, string newName)
+        {
+            OldName = oldName;
+            NewName = newName;
+        }
+    }
+
+}
