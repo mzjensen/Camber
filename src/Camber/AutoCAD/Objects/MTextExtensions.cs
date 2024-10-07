71129fa0ca6967f9c91138df82561f60e55aa76b
Zachri Jensen <zachrijensen@gmail.com>
2024-10-07 11:16:52 -0700

    Update MTextExtensions.cs

--- src/Camber/AutoCAD/Objects/MTextExtensions.cs
+++ src/Camber/AutoCAD/Objects/MTextExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Dynamo.Graph.Nodes;
+﻿using Autodesk.DesignScript.Runtime;
+using Dynamo.Graph.Nodes;
+using DynamoServices;
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Camber.Properties;
 using acDb = Autodesk.AutoCAD.DatabaseServices;
 using acDynApp = Autodesk.AutoCAD.DynamoApp.Services;
 using acDynNodes = Autodesk.AutoCAD.DynamoNodes;
@@ -11,56 +14,6 @@ namespace Camber.AutoCAD.Objects
 {
     public static class MText
     {
-        #region query methods
-        /// <summary>
-        /// Gets the name of the text style assigned to an MText.
-        /// </summary>
-        /// <param name="mText"></param>
-        /// <returns></returns>
-        [NodeCategory("Query")]
-        public static string TextStyle(this acDynNodes.MText mText) => GetString(mText, "TextStyleName");
-        #endregion
-
-        #region action methods
-        /// <summary>
-        /// Sets the text style for an MText.
-        /// </summary>
-        /// <param name="mText"></param>
-        /// <param name="textStyleName">The name of the text style to assign.</param>
-        /// <returns></returns>
-        public static acDynNodes.MText SetTextStyle(this acDynNodes.MText mText, string textStyleName)
-        {
-            if (string.IsNullOrEmpty(textStyleName))
-            {
-                throw new InvalidOperationException("Text style name is null or empty.");
-            }
-            
-            try
-            {
-                using (var ctx = new acDynApp.DocumentContext(acDynNodes.Document.Current.AcDocument))
-                {
-                    var textStyleTbl = (acDb.TextStyleTable) ctx.Transaction.GetObject(
-                        ctx.Database.TextStyleTableId,
-                        acDb.OpenMode.ForRead);
-                    if (!textStyleTbl.Has(textStyleName))
-                    {
-                        throw new InvalidOperationException("Text style does not exist.");
-                    }
-
-                    var styleId = textStyleTbl[textStyleName];
-                    var acMText = (AcMText) ctx.Transaction.GetObject(mText.InternalObjectId, acDb.OpenMode.ForWrite);
-                    acMText.TextStyleId = styleId;
-                    return mText;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException(ex.Message);
-            }
-            
-        }
-        #endregion
-
         #region helper methods
         internal static string GetString(
             acDynNodes.MText mText,
@@ -178,6 +131,68 @@ namespace Camber.AutoCAD.Objects
                 }
                 catch { throw; }
             }
+        }
+        #endregion
+
+        #region deprecated
+        /// <summary>
+        /// Gets the name of the text style assigned to an MText.
+        /// </summary>
+        /// <param name="mText"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        [NodeMigrationMapping(
+            "Camber.AutoCAD.Objects.MText.TextStyle",
+            "Autodesk.AutoCAD.DynamoNodes.MText.TextStyle")]
+        [NodeCategory("Query")]
+        public static string TextStyle(this acDynNodes.MText mText)
+        {
+            LogWarningMessageEvents.OnLogWarningMessage(string.Format(Resources.NODE_DEPRECATED_MIGRATION_MESSAGE, "MText.TextStyle"));
+            return GetString(mText, "TextStyleName");
+        }
+
+        /// <summary>
+        /// Sets the text style for an MText.
+        /// </summary>
+        /// <param name="mText"></param>
+        /// <param name="textStyleName">The name of the text style to assign.</param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        [NodeMigrationMapping(
+            "Camber.AutoCAD.Objects.MText.SetTextStyle",
+            "Autodesk.AutoCAD.DynamoNodes.MText.SetTextStyle")]
+        public static acDynNodes.MText SetTextStyle(this acDynNodes.MText mText, string textStyleName)
+        {
+            LogWarningMessageEvents.OnLogWarningMessage(string.Format(Resources.NODE_DEPRECATED_MIGRATION_MESSAGE, "MText.SetTextStyle"));
+
+            if (string.IsNullOrEmpty(textStyleName))
+            {
+                throw new InvalidOperationException("Text style name is null or empty.");
+            }
+
+            try
+            {
+                using (var ctx = new acDynApp.DocumentContext(acDynNodes.Document.Current.AcDocument))
+                {
+                    var textStyleTbl = (acDb.TextStyleTable)ctx.Transaction.GetObject(
+                        ctx.Database.TextStyleTableId,
+                        acDb.OpenMode.ForRead);
+                    if (!textStyleTbl.Has(textStyleName))
+                    {
+                        throw new InvalidOperationException("Text style does not exist.");
+                    }
+
+                    var styleId = textStyleTbl[textStyleName];
+                    var acMText = (AcMText)ctx.Transaction.GetObject(mText.InternalObjectId, acDb.OpenMode.ForWrite);
+                    acMText.TextStyleId = styleId;
+                    return mText;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+
         }
         #endregion
     }
