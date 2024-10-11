bc420b744f780d8eb205e6a640fe16be858157ff
Zachri Jensen <zachrijensen@gmail.com>
2024-10-10 20:30:37 -0700

    Reorder build responsibilities

--- src/Builder/CreatePackage.bat
+++ src/Builder/CreatePackage.bat
@@ -3,13 +3,18 @@ setlocal
 
 echo Starting to create package...
 
+set "BuildConfig=Debug"
 set "C3DVersion=2025"
 set "DynamoVersion=3.2"
 set "PackageName=Camber"
-set "SourceDir=%CD%\..\..\build\Debug"
+set "CamberSourceDir=%CD%\..\Camber\bin\%BuildConfig%"
+set "CamberUISourceDir=%CD%\..\CamberUI\bin\%BuildConfig%"
+set "AssistantSourceDir=%CD%\..\MigrationAssistant\bin\%BuildConfig%"
 set "DestDir=%APPDATA%\Autodesk\C3D %C3DVersion%\Dynamo\%DynamoVersion%\packages\%PackageName%"
 
-echo Source directory: %SourceDir%
+echo Camber source directory: %CamberSourceDir%
+echo Camber UI source directory: %CamberUISourceDir%
+echo Migration assistant source directory: %AssistantSourceDir%
 echo Package directory: %DestDir%
 
 if exist "%DestDir%" (
@@ -18,16 +23,17 @@ if exist "%DestDir%" (
 )
 
 mkdir "%DestDir%\bin"
-mkdir "%DestDir%\extra"
+mkdir "%DestDir%\extra\MigrationAssistant"
 
 echo Copying built source files...
-xcopy "%SourceDir%\*" "%DestDir%\bin" /s /i /y
+xcopy "%CamberSourceDir%\*" "%DestDir%\bin" /s /i /y
+xcopy "%CamberUISourceDir%\*" "%DestDir%\bin" /s /i /y
 
 echo Copying pkg.json...
 copy "%~dp0pkg.json" "%DestDir%" /y
 
 echo Copying extras...
-copy "%~dp0Camber.Migrations.xml" "%DestDir%\extra" /y
+xcopy "%AssistantSourceDir%\*" "%DestDir%\extra\MigrationAssistant" /s /i /y
 
 echo Finished creating '%PackageName%' package
 
