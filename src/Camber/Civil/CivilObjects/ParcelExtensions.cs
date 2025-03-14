7594a329704ca08ef1e1cb227fdfc277a2428897
Adam Reilly <adammreilly@yahoo.com>
2025-03-14 11:45:28 -0500

    added Parcel.ExteriorPolygon node. Creates a segmented polygon for the exterior loop of the parcel. Default spacing for tesselation is 1.0, allows for user input to change value.

--- src/Camber/Civil/CivilObjects/ParcelExtensions.cs
+++ src/Camber/Civil/CivilObjects/ParcelExtensions.cs
@@ -13,6 +13,11 @@ using Autodesk.AutoCAD.Runtime;
 using System.Security.Cryptography.Pkcs;
 using System;
 using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DynamoApp.Services;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Aec.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using ProtoCore.AST.ImperativeAST;
 #endregion
 
 namespace Camber.Civil.CivilObjects
@@ -21,7 +26,7 @@ namespace Camber.Civil.CivilObjects
     {
         #region properties
         /// <summary>
-        /// Returns the site that a arcel belongs to
+        /// Returns the site that a Parcel belongs to
         /// </summary>
         /// <param name="parcel"></param>
         /// <returns></returns>
@@ -119,6 +124,83 @@ namespace Camber.Civil.CivilObjects
             IList<Autodesk.DesignScript.Geometry.PolyCurve> retVal = PolyCurves(parcel);
             retVal.RemoveAt(0);
             return retVal;
+        }
+
+        /// <summary>
+        /// Returns a polygon representation of the exterior boundary of a parcel
+        /// </summary>
+        /// <param name="parcel"></param>
+        /// <param name="spacing">Distance along a curve to add points during decurving</param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static Autodesk.DesignScript.Geometry.Polygon ExteriorPolygon(this civDynNodes.Parcel parcel, double spacing = 1.0)
+        {
+            IList<Autodesk.DesignScript.Geometry.Curve> curves = new List<Autodesk.DesignScript.Geometry.Curve>();
+            Polyline poly = ExtractPolylines(parcel)[0];
+
+            // step thru the vertices and convert the segments to a new curve
+            for (int i = 0; i < poly.NumberOfVertices; i++)
+            {
+                Point3d startPt3d = poly.GetPoint3dAt(i);
+                Point3d endPt3d;
+                if (i == poly.NumberOfVertices - 1)
+                    endPt3d = poly.StartPoint;
+                else
+                    endPt3d = poly.GetPoint3dAt(i + 1);
+                // if the current segment is a curve, decurve it
+                if (poly.GetBulgeAt(i) != 0)
+                {
+                    double maxLength = poly.GetDistAtPoint(endPt3d);
+                    double curDistance = poly.GetDistAtPoint(startPt3d);
+                    while (curDistance < maxLength)
+                    {
+                        startPt3d = poly.GetPointAtDist(curDistance);
+                        Autodesk.DesignScript.Geometry.Point startPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(
+                            startPt3d.X,
+                            startPt3d.Y,
+                            startPt3d.Z);
+
+                        curDistance += spacing;
+                        if (curDistance > maxLength)
+                            curDistance = maxLength;
+                        endPt3d = poly.GetPointAtDist(curDistance);
+                        Autodesk.DesignScript.Geometry.Point endPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(
+                            endPt3d.X,
+                            endPt3d.Y,
+                            endPt3d.Z);
+                        if (!startPt3d.Equals(endPt3d))
+                        {
+                            Autodesk.DesignScript.Geometry.Curve curLine = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(startPt, endPt);
+                            curves.Add(curLine);
+                        }
+                    }
+                }
+                else
+                {
+                    Autodesk.DesignScript.Geometry.Point startPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(
+                        startPt3d.X,
+                        startPt3d.Y,
+                        startPt3d.Z);
+
+                    Autodesk.DesignScript.Geometry.Point endPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(
+                        endPt3d.X,
+                        endPt3d.Y,
+                        endPt3d.Z);
+                    if (!startPt3d.Equals(endPt3d))
+                    {
+                        Autodesk.DesignScript.Geometry.Curve curLine = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(startPt, endPt);
+                        curves.Add(curLine);
+                    }                    
+                }
+            }
+
+            Autodesk.DesignScript.Geometry.PolyCurve finalCurve = Autodesk.DesignScript.Geometry.PolyCurve.ByJoinedCurves(curves, 0, false, 0);
+            if (finalCurve.IsClosed == false)
+            {
+                finalCurve.CloseWithLine();
+            }
+            Autodesk.DesignScript.Geometry.Polygon pg = Autodesk.DesignScript.Geometry.Polygon.ByPoints(finalCurve.Points);
+            return pg;
         }
 
 
@@ -318,76 +400,94 @@ namespace Camber.Civil.CivilObjects
             return retVal;
         }
 
-        ///// <summary>
-        ///// Convert directly from parcel geometry to Curves, using mathematical recreation of arcs
-        ///// </summary>
-        ///// <param name="parcel"></param>
-        ///// <returns></returns>
-        //[IsVisibleInDynamoLibrary(false)]
-        //internal static IList<Autodesk.DesignScript.Geometry.Curve> GetPolyCurves(this civDynNodes.Parcel parcel)
-        //{
-        //    IList<Autodesk.DesignScript.Geometry.Curve> retVal = new List<Autodesk.DesignScript.Geometry.Curve>();
-        //    // get the COM object
-        //    dynamic oParcel = parcel.InternalDBObject.AcadObject;
-        //    // get the parcel loops from the COM object
-        //    dynamic loops = oParcel.ParcelLoops;
-        //    // loop the COM loops
-        //    foreach (dynamic loop in loops)
-        //    {
-        //        int count = loop.Count;
-        //        for (int i = 0; i < count; i++)
-        //        {
-        //            dynamic segElement = loop.Item(i);
-        //            double radius = 0;
-        //            double bulge = 0;
-        //            try
-        //            {
-        //                bulge = segElement.Bulge;
-        //                radius = segElement.Radius;
-        //            }
-        //            catch { }
-        //            Autodesk.DesignScript.Geometry.Point startPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(segElement.StartX, segElement.StartY);
-        //            Autodesk.DesignScript.Geometry.Point endPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(segElement.EndX, segElement.EndY);
-        //            if (radius == 0)
-        //            { // Line
-        //                Autodesk.DesignScript.Geometry.Line curLine = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(startPt, endPt);
-        //                retVal.Add(curLine);
-        //            }
-        //            else
-        //            { // Arc
-        //              // calculate the center point
-        //              //startPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(segElement.EndX, segElement.EndY);
-        //              //endPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(segElement.StartX, segElement.StartY);
-        //                double d = Math.Sqrt(((endPt.X - startPt.X) * (endPt.X - startPt.X)) + ((endPt.Y - startPt.Y) * (endPt.Y - startPt.Y)));
-        //                double Mx = (startPt.X + endPt.X) / 2;
-        //                double My = (startPt.Y + endPt.Y) / 2;
-        //                double sagitta = d / 2 * bulge;
-        //                double dx = endPt.X - startPt.X;
-        //                double dy = endPt.Y - startPt.Y;
-        //                Autodesk.DesignScript.Geometry.Vector unitVector = Autodesk.DesignScript.Geometry.Vector.ByCoordinates(-dy / d, dx / d, 0);
-        //                double R = d / 2 * ((1 + bulge * bulge) / (2 * bulge));
-        //                double Cx = Mx + Math.Sign(bulge) * (-dy / d) * radius;
-        //                double Cy = My + Math.Sign(bulge) * (dx / d) * radius;
-        //                Autodesk.DesignScript.Geometry.Point centerPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(Cx, Cy);
+        [IsVisibleInDynamoLibrary(true)]
+        public static Dictionary<string, Dictionary<string, object>> GetUserDefinedProperties(this civDynNodes.Parcel parcel)
+        {
 
-        //                // calculate the mid point
-        //                double theta = Math.Atan2(endPt.Y - startPt.Y, endPt.X - startPt.X);
-        //                double delta = 4 * Math.Atan(bulge);
-        //                double theta_m = theta + (delta / 2) + Math.CopySign(Math.PI / 2, bulge);
-        //                double Mx_arc = Cx + radius * Math.Cos(theta_m);
-        //                double My_arc = Cy + radius * Math.Sin(theta_m);
+            Dictionary<string, Dictionary<string, object>> dictionary = new Dictionary<string, Dictionary<string, object>>();
+            acDynNodes.Document document = acDynNodes.Document.Current;
+            using (acDynApp.DocumentContext ctx = new acDynApp.DocumentContext(document.AcDocument))
+                foreach (UDPClassification parcelUDPClassification in Autodesk.Civil.ApplicationServices.CivilDocument.GetCivilDocument(ctx.Database).ParcelUDPClassifications)
+                {
+                    Dictionary<string, object> dictionary2 = new Dictionary<string, object>();
+                    foreach (UDP uDP in parcelUDPClassification.UDPs)
+                    {
+                        if (uDP != null)
+                        {
+                            Autodesk.Civil.DatabaseServices.Parcel acParcel = ctx.Transaction.GetObject(parcel.InternalObjectId, OpenMode.ForRead) as Autodesk.Civil.DatabaseServices.Parcel;
+                            object userDefinedPropertyValueByName = Autodesk.Civil.DynamoNodes.Parcel.GetUserDefinedPropertyValueByName(acParcel, parcelUDPClassification.Name, uDP.Name);
+                            dictionary2.Add(uDP.Name, userDefinedPropertyValueByName);
+                        }
+                    }
+
+                    dictionary.Add(parcelUDPClassification.Name, dictionary2);
+                }
+
+            return dictionary;
+
+        }
+
+        /// <summary>
+        /// Applies the provided user defined data to the parcel
+        /// </summary>
+        /// <param name="parcel">The parcel to update</param>
+        /// <param name="classificationGroup">The classification group name</param>
+        /// <param name="propertyName">The user defined property key name</param>
+        /// <param name="propertyValue">The user defined property value</param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(true)]
+        public static civDynNodes.Parcel SetUserDefinedProperty(this civDynNodes.Parcel parcel, string classificationGroup, string propertyName, object propertyValue)
+        {
+            acDynNodes.Document document = acDynNodes.Document.Current;
+            using (acDynApp.DocumentContext ctx = new acDynApp.DocumentContext(document.AcDocument))
+            {
+                CivilDocument civilDocument = Autodesk.Civil.ApplicationServices.CivilDocument.GetCivilDocument(ctx.Database);
+                // verify that the classification group has been applied to the parcel
+                if ((propertyName != null) & (propertyValue != null))
+                    if (civilDocument.ParcelUDPClassifications.Contains(classificationGroup))
+                        foreach (UDPClassification parcelUDPClassification in Autodesk.Civil.ApplicationServices.CivilDocument.GetCivilDocument(ctx.Database).ParcelUDPClassifications)
+                        {
+                            foreach (UDP uDP in parcelUDPClassification.UDPs)
+                            {
+                                if (uDP != null)
+                                {
+                                    Autodesk.Civil.DatabaseServices.Parcel acParcel = ctx.Transaction.GetObject(parcel.InternalObjectId, OpenMode.ForWrite) as Autodesk.Civil.DatabaseServices.Parcel;
+                                    // verify that the user defined properties exist in the classification group
+                                    if (uDP.Name == propertyName)
+                                        // verify data type provided is allowed for the UDP value
+                                        if (!(uDP is UDPBoolean udp))
+                                        {
+                                            if (!(uDP is UDPDouble uDPDouble))
+                                            {
+                                                if (!(uDP is UDPEnumeration udp2))
+                                                {
+                                                    if (!(uDP is UDPInteger udp3))
+                                                    {
+                                                        if (uDP is UDPString udp4)
+                                                        {
+                                                            acParcel.SetUDPValue((UDPString)uDP, propertyValue.ToString());
+                                                        }
+                                                    }
+                                                    else
+                                                        acParcel.SetUDPValue((UDPInteger)uDP, (int)propertyValue);
+                                                }
+                                                else
+                                                    acParcel.SetUDPValue((UDPEnumeration)uDP, propertyValue.ToString());
+                                            }
+                                            else
+                                                acParcel.SetUDPValue((UDPDouble)uDP, (double)propertyValue);
+                                        }
+                                        else
+                                            acParcel.SetUDPValue((UDPBoolean)uDP, (bool)propertyValue);
+                                }
 
 
-        //                Autodesk.DesignScript.Geometry.Point midPt = Autodesk.DesignScript.Geometry.Point.ByCoordinates(Mx_arc, My_arc);
 
-        //                Autodesk.DesignScript.Geometry.Arc curCurve = Autodesk.DesignScript.Geometry.Arc.ByThreePoints(startPt, midPt, endPt);
-        //                retVal.Add(curCurve);
-        //            }
-        //        }
-        //    }
-
-        //    return retVal;
-        //}
+                            }
+                        }
+            }
+            return parcel;
+        }
 
 
 
